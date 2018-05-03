d00f8b68217a5231c3762ff4f2fbefbe7c394b14
SaadRegal <saadrg3@gmail.com>
2018-05-03 15:24:15 +0200

    added styles and framworks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+/*=== Angular Modules ===*/
+import {RouterModule, Routes} from '@angular/router';
 
+/*=== Component ===*/
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+
+  }];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AboutComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
