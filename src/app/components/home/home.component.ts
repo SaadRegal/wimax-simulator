b9d1ead5888032ada28060a5ca79344b2371c2dc
SaadRegal <saadrg3@gmail.com>
2018-05-18 00:57:58 +0100

    Revert "Revert "added new classes""
    This reverts commit cc505b5bcd25dfb59d13815858e099c041e8ec9e.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
 declare let $: any;
+declare let Chart: any;
 
 @Component({
   selector: 'app-home',
@@ -14,17 +15,80 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.initUI();
+
+
+
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
+
+
+
+
   }
 
   initUI() {
+    $( "input" ).on( "focus", function() {
+      // $('.sidebar.bottom').sidebar({"dimPage":"false"}).sidebar('show');
+      $('.sidebar.bottom').transition('fade in')
+    });
 
   }
 
   toggleTopBar() {
-    $('.sidebar').sidebar({
+    $('.sidebar.top').sidebar({
       "transition": "scale out",
       "silent":"true"
-    }).sidebar('toggle');
+    }).sidebar('show');
+
+  }
+
+  backToGraph(){
+    $('.sidebar.top').sidebar({
+      "transition":"scale out",
+    }).sidebar('hide').sidebar({
+      onHidden:$('.mainSegment').transition({
+        animation:"fade out",
+        onHidden:$('.charts').transition({duration:'1s'})
+      })
+        // .transition({
+        // "onComplete":$('.charts').transition('fade in')
+      // })
+    });
   }
 
 }
